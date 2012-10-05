7c2c6a8c16248bfa173903feeec54ac218bb158b
moen <moen@366.hopto.org>
2012-10-05 15:20:35 +0200

    Updated packing script to append version to a  jar file.

--- pack.sh
+++ pack.sh
@@ -11,11 +11,13 @@ od=`pwd`
 rm -fr "$outtmp/*"
 touch "$outtmp/.placeholder"
 
-#cp jar_data/* "$outtmp/"
-cp eclipse/Minecraft/bin/jaffas_01.png eclipse/Minecraft/bin/jaffas_logo.png "$outtmp"
+version=`grep -Ei '@Mod\(' src/common/jaffas/common/mod_jaffas.java  | sed -r 's/^.*( ?version ?= ?"(.?*)").*$/\2/'`
+#TODO check for failure in version detection
+
+cp eclipse/Minecraft/bin/jaffas_01.png eclipse/Minecraft/bin/jaffas_logo.png eclipse/Minecraft/bin/mcmod.info "$outtmp"
 cp -r reobf/minecraft/* "$outtmp"
 
 cd "$outtmp"
-zip -9r "../mod_jaffas.jar" ./*
+zip -9r "../mod_jaffas_$version.jar" ./*
 
 cd "$od"
