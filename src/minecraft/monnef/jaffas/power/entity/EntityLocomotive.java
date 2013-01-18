d166ac5faef71fa4acb31e3c92507b3307b5fedc
moen <moen@366.hopto.org>
2013-01-19 00:10:33 +0100

    First version of locomotive.

---
+++ src/minecraft/monnef/jaffas/power/entity/EntityLocomotive.java
@@ -0,0 +1,14 @@
+package monnef.jaffas.power.entity;
+
+import net.minecraft.entity.item.EntityMinecart;
+import net.minecraft.world.World;
+
+public class EntityLocomotive extends EntityMinecart {
+    public EntityLocomotive(World par1World, double par2, double par4, double par6, int type) {
+        super(par1World, par2, par4, par6, type);
+    }
+
+    public EntityLocomotive(World w) {
+        super(w);
+    }
+}
