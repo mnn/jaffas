89451c970044f1cb93b582d25116637f66003d27
moen <moen@366.hopto.org>
2013-05-14 15:44:21 +0200

    Some refactoring of tree generator (getting rid of de-obfuscated names).

--- src/minecraft/monnef/jaffas/trees/common/WorldGenFruitTrees.java
+++ src/minecraft/monnef/jaffas/trees/common/WorldGenFruitTrees.java
@@ -49,80 +49,74 @@ public class WorldGenFruitTrees extends WorldGenerator {
         this.leavesID = leavesID;
     }
 
-    public boolean generate(World par1World, Random par2Random, int par3, int par4, int par5) {
-        int var6 = par2Random.nextInt(3) + this.minTreeHeight;
-        boolean var7 = true;
+    public boolean generate(World world, Random random, int x, int y, int z) {
+        int treeHeight = random.nextInt(3) + this.minTreeHeight;
+        boolean doGenerate = true;
 
-        if (par4 >= 1 && par4 + var6 + 1 <= 256) {
-            int var8;
-            byte var9;
-            int var11;
-            int var12;
+        // check
+        if (y >= 1 && y + treeHeight + 1 <= 256) {
+            for (int stemY = y; stemY <= y + 1 + treeHeight; ++stemY) {
+                byte radius = 1;
 
-            for (var8 = par4; var8 <= par4 + 1 + var6; ++var8) {
-                var9 = 1;
-
-                if (var8 == par4) {
-                    var9 = 0;
+                if (stemY == y) {
+                    radius = 0;
                 }
 
-                if (var8 >= par4 + 1 + var6 - 2) {
-                    var9 = 2;
+                if (stemY >= y + 1 + treeHeight - 2) {
+                    radius = 2;
                 }
 
-                for (int var10 = par3 - var9; var10 <= par3 + var9 && var7; ++var10) {
-                    for (var11 = par5 - var9; var11 <= par5 + var9 && var7; ++var11) {
-                        if (var8 >= 0 && var8 < 256) {
-                            var12 = par1World.getBlockId(var10, var8, var11);
+                for (int xx = x - radius; xx <= x + radius && doGenerate; ++xx) {
+                    for (int zz = z - radius; zz <= z + radius && doGenerate; ++zz) {
+                        if (stemY >= 0 && stemY < 256) {
+                            int blockId = world.getBlockId(xx, stemY, zz);
 
-                            Block block = Block.blocksList[var12];
+                            Block block = Block.blocksList[blockId];
 
-                            if (var12 != 0 &&
-                                    !block.isLeaves(par1World, var10, var8, var11) &&
-                                    var12 != Block.grass.blockID &&
-                                    var12 != Block.dirt.blockID &&
-                                    !block.isWood(par1World, var10, var8, var11)) {
-                                var7 = false;
+                            if (blockId != 0 &&
+                                    !block.isLeaves(world, xx, stemY, zz) &&
+                                    blockId != Block.grass.blockID &&
+                                    blockId != Block.dirt.blockID &&
+                                    !block.isWood(world, xx, stemY, zz)) {
+                                doGenerate = false;
                             }
                         } else {
-                            var7 = false;
+                            doGenerate = false;
                         }
                     }
                 }
             }
 
-            if (!var7) {
+            if (!doGenerate) {
                 return false;
             } else {
-                var8 = par1World.getBlockId(par3, par4 - 1, par5);
+                int blockUnder = world.getBlockId(x, y - 1, z);
 
-                if ((var8 == Block.grass.blockID || var8 == Block.dirt.blockID) && par4 < 256 - var6 - 1) {
-                    this.setBlock(par1World, par3, par4 - 1, par5, Block.dirt.blockID);
-                    var9 = 3;
+                if ((blockUnder == Block.grass.blockID || blockUnder == Block.dirt.blockID) && y < 256 - treeHeight - 1) {
+                    this.setBlock(world, x, y - 1, z, Block.dirt.blockID);
+                    byte topHeight = 3;
                     byte var18 = 0;
-                    int var13;
-                    int var14;
-                    int var15;
+                    int crownRadius;
 
-                    for (var11 = par4 - var9 + var6; var11 <= par4 + var6; ++var11) {
-                        var12 = var11 - (par4 + var6);
-                        var13 = var18 + 1 - var12 / 2;
+                    for (int yy = y - topHeight + treeHeight; yy <= y + treeHeight; ++yy) {
+                        int currentTopLevel = yy - (y + treeHeight);
+                        crownRadius = var18 + 1 - currentTopLevel / 2;
 
-                        for (var14 = par3 - var13; var14 <= par3 + var13; ++var14) {
-                            var15 = var14 - par3;
+                        for (int xx = x - crownRadius; xx <= x + crownRadius; ++xx) {
+                            int xxShift = xx - x;
 
-                            for (int var16 = par5 - var13; var16 <= par5 + var13; ++var16) {
-                                int var17 = var16 - par5;
+                            for (int zz = z - crownRadius; zz <= z + crownRadius; ++zz) {
+                                int zzShift = zz - z;
 
-                                Block block = Block.blocksList[par1World.getBlockId(var14, var11, var16)];
+                                Block block = Block.blocksList[world.getBlockId(xx, yy, zz)];
 
-                                if ((Math.abs(var15) != var13 || Math.abs(var17) != var13 || par2Random.nextInt(2) != 0 && var12 != 0) &&
-                                        (block == null || block.canBeReplacedByLeaves(par1World, var14, var11, var16))) {
+                                if ((Math.abs(xxShift) != crownRadius || Math.abs(zzShift) != crownRadius || random.nextInt(2) != 0 && currentTopLevel != 0) &&
+                                        (block == null || block.canBeReplacedByLeaves(world, xx, yy, zz))) {
 
                                     int chosenLeavesMeta;
                                     int chosenLeavesID;
 
-                                    if (par2Random.nextInt(3) == 0) {
+                                    if (random.nextInt(3) == 0) {
                                         chosenLeavesID = this.leavesID;
                                         chosenLeavesMeta = this.metaLeaves;
                                     } else {
@@ -130,37 +124,37 @@ public class WorldGenFruitTrees extends WorldGenerator {
                                         chosenLeavesMeta = 0;
                                     }
 
-                                    this.setBlockAndMetadata(par1World, var14, var11, var16, JaffasTrees.leavesList.get(0).leavesID, 0);
+                                    this.setBlockAndMetadata(world, xx, yy, zz, JaffasTrees.leavesList.get(0).leavesID, 0);
                                     TileEntityFruitLeaves te = new TileEntityFruitLeaves(chosenLeavesID, chosenLeavesMeta);
-                                    par1World.setBlockTileEntity(var14, var11, var16, te);
+                                    world.setBlockTileEntity(xx, yy, zz, te);
                                 }
                             }
                         }
                     }
 
-                    for (var11 = 0; var11 < var6; ++var11) {
-                        var12 = par1World.getBlockId(par3, par4 + var11, par5);
+                    for (int yy = 0; yy < treeHeight; ++yy) {
+                        int blockId = world.getBlockId(x, y + yy, z);
 
-                        Block block = Block.blocksList[var12];
+                        Block block = Block.blocksList[blockId];
 
-                        if (var12 == 0 || block == null || block.isLeaves(par1World, par3, par4 + var11, par5)) {
-                            this.setBlockAndMetadata(par1World, par3, par4 + var11, par5, Block.wood.blockID, this.metaWood);
+                        if (blockId == 0 || block == null || block.isLeaves(world, x, y + yy, z)) {
+                            this.setBlockAndMetadata(world, x, y + yy, z, Block.wood.blockID, this.metaWood);
 
-                            if (this.vinesGrow && var11 > 0) {
-                                if (par2Random.nextInt(3) > 0 && par1World.isAirBlock(par3 - 1, par4 + var11, par5)) {
-                                    this.setBlockAndMetadata(par1World, par3 - 1, par4 + var11, par5, Block.vine.blockID, 8);
+                            if (this.vinesGrow && yy > 0) {
+                                if (random.nextInt(3) > 0 && world.isAirBlock(x - 1, y + yy, z)) {
+                                    this.setBlockAndMetadata(world, x - 1, y + yy, z, Block.vine.blockID, 8);
                                 }
 
-                                if (par2Random.nextInt(3) > 0 && par1World.isAirBlock(par3 + 1, par4 + var11, par5)) {
-                                    this.setBlockAndMetadata(par1World, par3 + 1, par4 + var11, par5, Block.vine.blockID, 2);
+                                if (random.nextInt(3) > 0 && world.isAirBlock(x + 1, y + yy, z)) {
+                                    this.setBlockAndMetadata(world, x + 1, y + yy, z, Block.vine.blockID, 2);
                                 }
 
-                                if (par2Random.nextInt(3) > 0 && par1World.isAirBlock(par3, par4 + var11, par5 - 1)) {
-                                    this.setBlockAndMetadata(par1World, par3, par4 + var11, par5 - 1, Block.vine.blockID, 1);
+                                if (random.nextInt(3) > 0 && world.isAirBlock(x, y + yy, z - 1)) {
+                                    this.setBlockAndMetadata(world, x, y + yy, z - 1, Block.vine.blockID, 1);
                                 }
 
-                                if (par2Random.nextInt(3) > 0 && par1World.isAirBlock(par3, par4 + var11, par5 + 1)) {
-                                    this.setBlockAndMetadata(par1World, par3, par4 + var11, par5 + 1, Block.vine.blockID, 4);
+                                if (random.nextInt(3) > 0 && world.isAirBlock(x, y + yy, z + 1)) {
+                                    this.setBlockAndMetadata(world, x, y + yy, z + 1, Block.vine.blockID, 4);
                                 }
                             }
                         }
