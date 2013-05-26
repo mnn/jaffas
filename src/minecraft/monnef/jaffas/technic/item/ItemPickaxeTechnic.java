6a8b77ecffbbf6505e0aba55660e949769cb23ce
moen <moen@366.hopto.org>
2013-05-26 20:28:39 +0200

    Fixed jaffarrol pickaxe to be able to mine same blocks as diamond pickaxe.

--- src/minecraft/monnef/jaffas/technic/item/ItemPickaxeTechnic.java
+++ src/minecraft/monnef/jaffas/technic/item/ItemPickaxeTechnic.java
@@ -5,10 +5,23 @@
 
 package monnef.jaffas.technic.item;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ItemPickaxeTechnic extends ItemTechnicTool {
     public ItemPickaxeTechnic(int id, int textureOffset, EnumToolMaterial material) {
         super(id, textureOffset, material);
+    }
+
+    @Override
+    public float getStrVsBlock(ItemStack stack, Block block, int metadata) {
+        return Item.pickaxeDiamond.getStrVsBlock(stack, block, metadata);
+    }
+
+    @Override
+    public boolean canHarvestBlock(Block block) {
+        return Item.pickaxeDiamond.canHarvestBlock(block);
     }
 }
