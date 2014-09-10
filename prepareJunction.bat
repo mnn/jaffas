5b567768ddc7b3b0e8faffed59f0fe27cf054ac3
moen <moen@366.hopto.org>
2014-09-10 23:14:40 +0200

    Added core assets to junctions script.

--- prepareJunction.bat
+++ prepareJunction.bat
@@ -1,5 +1,6 @@
 mklink /J src\main\java\monnef\core core\src\main\java\monnef\core
 mklink /J src\main\scala\monnef\core core\src\main\scala\monnef\core
 mklink /J src\main\java\buildcraft core\src\main\java\buildcraft
+mklink /J "src\main\resources\assets\monnef core" "core\src\main\resources\assets\monnef core"
 
 pause
