542ac9df23f0985c6c1ddb040c4752166f74d8b4
moen <moen@366.hopto.org>
2014-09-11 13:58:17 +0200

    Updated packing script.

--- pack.bash
+++ pack.bash
@@ -12,20 +12,22 @@ output_dir="jar_output"
 dir_all="$output_dir/all"
 dir_core="$output_dir/core"
 dir_jaffas="$output_dir/jaffas"
+current_phase=""
 
 function ensureDir {
 	if [ \! -d "$1" ]; then
 		echo "Creating \"$1\"."
-		mkdir "$1"
+		mkdir -p "$1"
 	fi
 }
 
 function printStart {
-	echo -n "$1 ... "
+	current_phase="$1"
+	echo "$current_phase ... "
 }
 
 function printDone {
-	echo "done"
+	echo "$current_phase ... done"
 }
 
 
@@ -51,4 +53,47 @@ printStart "Unpacking jar"
 unzip -q "$combined_jar" -d "$dir_all"
 printDone
 
-# TODO: moving to appropriate directories ($dir_core and $dir_jaffas) and zipping
+printStart "Categorizing directories"
+cd "$current"
+
+if [ -d  "$dir_all/assets/monnef core" ]; then
+	ensureDir "$dir_core/assets"
+	mv "$dir_all/assets/monnef core" "$dir_core/assets"
+fi
+mv "$dir_all/assets" "$dir_jaffas"
+
+# APIs
+mv "$dir_all/buildcraft" "$dir_core"
+mv "$dir_all/forestry" "$dir_jaffas"
+mv "$dir_all/powercrystals" "$dir_jaffas"
+
+# mostly my classes
+ensureDir "$dir_core/monnef"
+mv "$dir_all/monnef/core" "$dir_core/monnef"
+mv "$dir_all/monnef/external" "$dir_core/monnef"
+mv "$dir_all/monnef" "$dir_jaffas"
+
+# rest
+mv "$dir_all"/* "$dir_jaffas"
+printDone
+
+printStart "Creating core manifest"
+dir_core_manifest="$dir_core/META-INF"
+ensureDir "$dir_core_manifest"
+echo -e 'Manifest-Version: 1.0\nFMLCorePlugin: monnef.core.MonnefCorePlugin\n' > "$dir_core_manifest/MANIFEST.MF"
+printDone
+
+printStart "Packing core"
+cd "$current"
+cd "$dir_core"
+zip -q -9r "../monnef_core_${core_version}.jar" ./*
+cd "$current"
+printDone
+
+printStart "Packing mod"
+cd "$current"
+cd "$dir_jaffas"
+zip -q -9r "../jaffas_${mod_version}.jar" ./*
+cd "$current"
+printDone
+
