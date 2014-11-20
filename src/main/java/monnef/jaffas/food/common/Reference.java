769c29262a4e56b2e94050a0e30d36ec6bcd8677
moen <moen@366.hopto.org>
2014-11-20 19:20:29 +0100

    Bumped version.

--- src/main/java/monnef/jaffas/food/common/Reference.java
+++ src/main/java/monnef/jaffas/food/common/Reference.java
@@ -6,7 +6,7 @@
 package monnef.jaffas.food.common;
 
 public class Reference {
-    public static final String Version = "0.7.2";
+    public static final String Version = "0.7.3";
     public static final String ModId = "Jaffas";
     public static final String ModName = "Jaffas and more!";
     public static final String ITEM_NAME_PREFIX = "jaffas";
